27922b830ee642f94010281f7379e75ae1e7a15d
JMSBRN <zakhavai@gmail.com>
2023-04-24 12:44:47 +0300

    fix: updated api endpoints functional

--- pages/api/users/[userId].tsx
+++ pages/api/users/[userId].tsx
@@ -1,25 +1,9 @@
 /* eslint-disable import/no-anonymous-default-export */
 import clientPromise from '@/lib/mongodb';
-import { ObjectId } from 'mongodb';
+import { updateMongoDb } from '@/utils/apiUtils';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const { userId } = req.query;
-    const rgx = /^[0-9a-fA-F]{24}$/.test(userId as string);
-    if(rgx) {
-      const client = await clientPromise;
-      const db = client.db('painters');
-      const user = await db
-      .collection('users')
-      .findOne({ _id: new ObjectId(userId as string) });
-      // regex ?
-      
-      if (user) {
-          res.status(200).json(user);
-        } else {
-          res.status(404).json({ message: 'User not found' });
-        }
-    } else {
-      res.status(404).json({ message: 'Not valid id' });
-    }
+  const client = await clientPromise;
+  updateMongoDb(req, res, client, 'painters', 'users');
 };
