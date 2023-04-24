16431adb6c2c34cacbfc12e348d6a281c3067f82
JMSBRN <zakhavai@gmail.com>
2023-04-24 11:33:39 +0300

    fix: created regex for  id user

--- pages/api/users/[userId].tsx
+++ pages/api/users/[userId].tsx
@@ -4,16 +4,22 @@ import { ObjectId } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const {userId } = req.query;
-    const client = await clientPromise;
-    const db = client.db('painters');
-    const user = await db
-    .collection('users')
-    .findOne({ _id: new ObjectId(userId as string) });
-    // regex ?
-    if (user) {
-        res.status(200).json(user);
-      } else {
-        res.status(404).json({ message: 'User not found' });
-      }
+    const { userId } = req.query;
+    const rgx = /^[0-9a-fA-F]{24}$/.test(userId as string);
+    if(rgx) {
+      const client = await clientPromise;
+      const db = client.db('painters');
+      const user = await db
+      .collection('users')
+      .findOne({ _id: new ObjectId(userId as string) });
+      // regex ?
+      
+      if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({ message: 'User not found' });
+        }
+    } else {
+      res.status(404).json({ message: 'Not valid id' });
+    }
 };
