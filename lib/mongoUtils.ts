efa7db71d52f956ee9c427d0c8e54be236fba047
JMSBRN <zakhavai@gmail.com>
2023-05-02 13:02:38 +0300

    fix: updated upload functional

--- lib/mongoUtils.ts
+++ lib/mongoUtils.ts
@@ -1,5 +1,9 @@
+import { createReadStream, unlink } from 'fs';
 import { readdir } from 'fs/promises';
-import { MongoClient, Db } from 'mongodb';
+import { MongoClient, Db, GridFSBucket } from 'mongodb';
+import { join } from 'path';
+import { cwd } from 'process';
+import mime from 'mime-types';
 
 const uri = process.env.MONGODB_URI as string;
 const dbName = process.env.MONGODB_DB as string;
@@ -29,4 +33,22 @@ export const getfileNamesFromDir = async (path: string) => {
      } else {
         return;
      }
+};
+
+export const uploadGridFSFile = async (pathToDir: string, db: Db, fileName: string) => {
+  const bucket = new GridFSBucket(db, { bucketName: 'testo'});
+  const files = await getfileNamesFromDir(join(cwd(), '/public/images'));
+  if(files?.length) {
+    const filePath = join(cwd(), pathToDir, `${files[0]}`);
+    const contentType  = mime.lookup(files[0]) as string;
+    const readStream = createReadStream(filePath).pipe(bucket.openUploadStream(fileName, {
+      contentType: contentType
+    }));
+    readStream.on('close', () => {
+      unlink(filePath, (er) => {
+        if(er) throw er;
+      });
+    });
+ 
+  }
 };
--- pages/help.tsx
+++ pages/help.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import { connectToDatabase, getfileNamesFromDir } from '@/lib/mongoUtils';
-import { createReadStream, unlink, unlinkSync } from 'fs';
-import { GridFSBucket } from 'mongodb';
-import { join } from 'path';
-import { cwd } from 'process';
-import mime from 'mime-types';
+import { connectToDatabase, uploadGridFSFile } from '@/lib/mongoUtils';
 
 const Help = ({ data }: { data: {}} ) => {  
   return (
@@ -19,23 +14,7 @@ export default Help;
 export const getStaticProps = async() => {
   const data = {};
   const { db } = await connectToDatabase();
-  const bucket = new GridFSBucket(db, { bucketName: 'testo'});
-  const files = await getfileNamesFromDir(join(cwd(), '/public/images'));
-  if(files?.length) {
-    const filePath = join(cwd(), '/public/images', `${files[0]}`);
-    const contentType  = mime.lookup(files[0]) as string;
-    const readStream = createReadStream(filePath).pipe(bucket.openUploadStream('test', {
-      contentType: contentType
-    }));
-    readStream.on('close', () => {
-      unlink(filePath, (er) => {
-        if(er) throw er;
-      });
-    });
- 
-  } else {
-
-  }
+  await uploadGridFSFile('/public/images', db, 'user_f');   
   return {
     props: data
   };
