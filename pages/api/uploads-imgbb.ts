80f296deb67af23974205821243da51a419fd218
JMSBRN <zakhavai@gmail.com>
2023-06-09 10:34:18 +0300

    fix: updated styles for images in  painter comp.

--- pages/api/uploads-imgbb.ts
+++ pages/api/uploads-imgbb.ts
@@ -15,7 +15,6 @@ const handler = async (req:NextApiRequest, res: NextApiResponse) => {
     return res.status(204).end();
   } else {
     const { resultImgBb, id } = body;
-    console.log(resultImgBb);
     const { display_url, url, title } = resultImgBb.data;
     const newData = { id, display_url, url, title };
     const result = await addDataToCollection('imgBB', newData);
