09d8e66cf4ab5f083c4c9539027c7a769e2a8cd9
JMSBRN <zakhavai@gmail.com>
2023-07-13 17:49:35 +0300

    fix: upadetd Home comp.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import styles from '../styles/home.module.scss';
 
-const Home = () => {  
-  
+const Home = () => {
+  const { mainContainer, welcomeBlock, secondTitlesContainer } = styles;
+
   return (
     <>
-      <div>
-       home
+      <div className={mainContainer}>
+        <div className={welcomeBlock}>
+          <h1>Welcome to the Painters Gallery!</h1>
+            <div className={secondTitlesContainer}>
+              <p>
+                Immerse yourself in a world of vibrant colors, captivating
+                brushstrokes, and artistic expression. Our gallery is a haven for
+                art enthusiasts and a platform that celebrates the talent and
+                creativity of painters from around the globe.
+              </p>
+              <p>
+                Step into a realm where imagination takes form on canvas, where
+                every stroke tells a story, and where emotions are translated into
+                breathtaking visual masterpieces. From classic landscapes to
+                abstract marvels, our diverse collection showcases a rich tapestry
+                of artistic styles and techniques.
+              </p>
+              <p>
+                As you wander through our gallery, allow the brushstrokes to guide
+                you on a journey of exploration and introspection. Each painting
+                whispers its own narrative, inviting you to pause, reflect, and
+                connect with the artists vision.
+              </p>
+              <p>
+                Whether you are a seasoned collector, an aspiring artist, or simply
+                a lover of beauty, our gallery offers a sanctuary for inspiration
+                and contemplation. Discover new talents and established masters,
+                each with their own unique voice and perspective,
+              </p>
+            </div>
+        </div>
       </div>
     </>
   );
