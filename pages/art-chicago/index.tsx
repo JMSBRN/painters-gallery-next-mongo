9c3c790bcf5ee5e922d4a2ce108c27f202e46e71
JMSBRN <zakhavai@gmail.com>
2023-07-20 18:04:01 +0300

    feat: created pagination "art-chicago"

--- pages/art-chicago/index.tsx
+++ pages/art-chicago/index.tsx
@@ -1,16 +1,39 @@
 import { Artworks } from '@/styles/art-chicago/iInterfaces';
 import Image from 'next/image';
+import PaginationArtChicago from './components/pagination/PaginationArtChicago';
 import React from 'react';
+import SearchForm from './components/search-form/SearchForm';
 import { getArtWorks } from '@/styles/art-chicago/artApiUtils';
 import styles from '../../styles/artChicago.module.scss';
 
 const ArtChicago = ({ fetchedData }: { fetchedData: Artworks }) => {
-  const { mainContainer, artWorks, imageConatiner } = styles;
+  const { mainContainer, artWorks, imageConatiner, searchFormContainer } = styles;
 
-  const { data } = fetchedData;
+  const { data, pagination } = fetchedData;
+  const { total, total_pages, current_page } = pagination;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const handleChangePage = (_: any, page: number) => {
+    console.log(page);
+  };
     
   return (
     <div className={mainContainer}>
+      <div className="total">total: {total}</div>
+      <PaginationArtChicago 
+      totalPages={total_pages}
+      currentPage={current_page}
+      handleChangePage={handleChangePage}
+      />
+      <div className={searchFormContainer}>
+        <SearchForm  handleSubmit={handleSubmit} handleChange={handleChange} />
+      </div>
       <div className={artWorks}>
       {data.map( el => (
         <div key={el.id}>
@@ -19,7 +42,7 @@ const ArtChicago = ({ fetchedData }: { fetchedData: Artworks }) => {
             src={`https://www.artic.edu/iiif/2/${el.image_id}/full/400,/0/default.jpg`}
             width={100}
             height={120}
-            alt={el.thumbnail.alt_text}
+            alt='alt data'
             />
           </div>
         </div>
